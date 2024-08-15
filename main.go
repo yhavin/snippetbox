38be1b3631e2bcd9baff320f346a20824d32fea3
yhavin <y.havin@gmail.com>
2024-08-15 16:16:30 -0400

    Update http error handling

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -19,6 +19,12 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
