8877286827e15c374fd067374a55babbe67d3a24
yhavin <y.havin@gmail.com>
2024-08-21 17:41:27 -0400

    Add protection for authorised routes

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -37,3 +37,16 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
+
+		next.ServeHTTP(w, r)
+	})
+}
