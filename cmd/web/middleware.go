b5d8c1c52c403926026ffa43aef48ca8c12f2e70
yhavin <y.havin@gmail.com>
2024-08-26 10:17:35 -0400

    Add middleware unit tests

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -12,7 +12,7 @@ func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
-		w.Header().Set("X-Content-Type-Options", "no-sniff")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
 
